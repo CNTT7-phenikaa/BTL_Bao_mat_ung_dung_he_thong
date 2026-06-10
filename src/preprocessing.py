7a72d7d103ccc276d61e85dcec10eecd92e59916
Vu Thi Khanh Van <23010679@st.phenikaa-uni.edu.vn>
2026-06-10 18:57:35 +0700

    Update preprocessing

---
+++ src/preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+def remove_duplicates(df):
+    return df.drop_duplicates()
+
+def remove_infinite(df):
+    df = df.replace([np.inf, -np.inf], np.nan)
+    return df.dropna()
+
+def remove_constant_features(df):
+    constant_cols = [
+        col for col in df.columns
+        if df[col].nunique() == 1
+    ]
+    return df.drop(columns=constant_cols)
+
+def split_data(X, y):
+    from sklearn.model_selection import train_test_split
+
+    return train_test_split(
+        X, y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
